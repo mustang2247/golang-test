848b70d00e902dda340e200bc8127c7171855027
mustangkong <mustang2247@163.com>
2017-08-17 19:06:01 +0800

    add message

--- golang-test/maingoroutine.go
+++ golang-test/maingoroutine.go
@@ -1,15 +1,29 @@
+// go 并发
 package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main()  {
 	// go routine
-	go Go()
-	time.Sleep(2 * time.Second)
-	fmt.Println("hell")
+	//go Go()
+	//time.Sleep(2 * time.Second)
+	//fmt.Println("hell")
+
+	c := make(chan bool)
+	go func() {
+		fmt.Println("gogogo")
+		c <- true
+		close(c)//使用for range必须明确关闭
+	}()
+	//<-c
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
+	// 单项通道和双向通道
 }
 
 func Go() {
