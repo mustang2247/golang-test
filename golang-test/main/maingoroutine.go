366952f33ccc14678de18a85b41b83eabc60ff87
mustangkong <mustang2247@163.com>
2017-08-24 11:17:16 +0800

    update project

--- golang-test/main/maingoroutine.go
+++ golang-test/main/maingoroutine.go
@@ -11,12 +11,16 @@ func main()  {
 	fmt.Println(runtime.NumCPU())
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	wg := sync.WaitGroup{}
-	wg.Add(10)
 	for i := 0; i<10; i++ {
-		go Go(&wg, i)
+		wg := sync.WaitGroup{}
+		wg.Add(10)
+		for i := 0; i<10; i++ {
+			go Go(&wg, i)
+		}
+		wg.Wait()
+
+		fmt.Println(" \n ")
 	}
-	wg.Wait()
 }
 
 func Go(wg *sync.WaitGroup, index int) {
